6c63f7f8437f70897f0a2977607a07899d0030f9
Mehedi hasan <mmehedihasanjoyv@gmail.com>
2025-02-06 23:39:58 +0600

    first commit

---
+++ src/app/modules/user/user.route.ts
@@ -0,0 +1,61 @@
+import express, { NextFunction, Response,Request } from 'express';
+import auth from '../../middlewares/auth';
+import validateRequest from '../../middlewares/validateRequest';
+import { createAdminValidationSchema } from '../Admin/admin.validation';
+import { createFacultyValidationSchema } from '../Faculty/faculty.validation';
+import { createStudentValidationSchema } from './../student/student.validation';
+import { USER_ROLE } from './user.constant';
+import { UserControllers } from './user.controller';
+import { UserValidation } from './user.validation';
+import { upload } from '../../utils/sendImageToCloudinary';
+
+const router = express.Router();
+
+router.post(
+  '/create-student',
+  auth(USER_ROLE.admin,USER_ROLE.superAdmin),
+  upload.single('file'),
+  (req: Request, res: Response, next: NextFunction) => {
+    req.body = JSON.parse(req.body.data);
+    next()
+  },
+  validateRequest(createStudentValidationSchema),
+  UserControllers.createStudent,
+);
+
+router.post(
+  '/create-faculty',
+  auth(USER_ROLE.admin,USER_ROLE.superAdmin),
+  upload.single('file'),
+  (req: Request, res: Response, next: NextFunction) => {
+    req.body = JSON.parse(req.body.data);
+    next()
+  },
+  validateRequest(createFacultyValidationSchema),
+  UserControllers.createFaculty,
+);
+
+router.post(
+  '/create-admin',
+  auth(USER_ROLE.admin,USER_ROLE.superAdmin),
+  upload.single('file'),
+  (req: Request, res: Response, next: NextFunction) => {
+    req.body = JSON.parse(req.body.data);
+    next()
+  },
+  validateRequest(createAdminValidationSchema),
+  UserControllers.createAdmin,
+);
+router.get(
+  '/me',
+  auth(USER_ROLE.admin,USER_ROLE.superAdmin,USER_ROLE.faculty,USER_ROLE.student),
+  UserControllers.getMe,
+);
+router.post(
+  '/change-status/:id',
+  auth(USER_ROLE.admin,USER_ROLE.superAdmin),
+  validateRequest(UserValidation.changeStatusValidationSchema),
+  UserControllers.changeStatus,
+);
+
+export const UserRoutes = router;
